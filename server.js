b93c2c76b4562501488a4e9de7265e7aebeb25a2
Feng <yinfeng@wanleyun.com>
2016-01-18 23:12:59 +0800

    update server

--- server.js
+++ server.js
@@ -2,22 +2,27 @@ var webpack = require('webpack')
 var webpackDevMiddleware = require('webpack-dev-middleware')
 var webpackHotMiddleware = require('webpack-hot-middleware')
 var config = require('./webpack.config')
+var exec = require('child_process').exec
 
-var app = new (require('express'))()
+var app = new(require('express'))()
 var port = 3000
 
 var compiler = webpack(config)
-app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
+app.use(webpackDevMiddleware(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+}))
 app.use(webpackHotMiddleware(compiler))
 
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + '/h5/index.html')
+    res.sendFile(__dirname + '/h5/index.html')
 })
 
 app.listen(port, function(error) {
-  if (error) {
-    console.error(error)
-  } else {
-    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
-  }
+    if (error) {
+        console.error(error)
+    } else {
+        console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
+        exec('open http://localhost:' + port);
+    }
 })
