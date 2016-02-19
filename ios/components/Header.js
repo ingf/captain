914d3bd123ebd50926497e9b4780ce210e7361a9
Feng <yinfeng@wanleyun.com>
2016-02-19 18:00:21 +0800

    js format

--- ios/components/Header.js
+++ ios/components/Header.js
@@ -1,55 +1,55 @@
 import React, {
-    PanResponder,
-    Component,
-    View,
-    Text,
-    Image,
-    TextInput
+  PanResponder,
+  Component,
+  View,
+  Text,
+  Image,
+  TextInput
 }
 from 'react-native'
 import TodoTextInput from './TodoTextInput'
 
 export default class Header extends Component {
 
-    onChangeText(text) {
-        this.props.addTodo(text)
-    }
+  onChangeText(text) {
+    this.props.addTodo(text)
+  }
 
-    componentWillMount() {
-        this._panResponder = PanResponder.create({
-            onStartShouldSetPanResponder: () => true,
-            onMoveShouldSetPanResponder: () => true,
-            onPanResponderGrant: this._highlight.bind(this),
-            onPanResponderMove: this._handlePanResponderMove,
-            onPanResponderRelease: this._handlePanResponderEnd,
-            onPanResponderTerminate: this._handlePanResponderEnd,
-        })
-    }
+  componentWillMount() {
+    this._panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderGrant: this._highlight.bind(this),
+      onPanResponderMove: this._handlePanResponderMove,
+      onPanResponderRelease: this._handlePanResponderEnd,
+      onPanResponderTerminate: this._handlePanResponderEnd,
+    })
+  }
 
-    _highlight(e, gestureState) {
-        console.log(e)
-        console.log(gestureState)
-    }
+  _highlight(e, gestureState) {
+    console.log(e)
+    console.log(gestureState)
+  }
 
-    _handlePanResponderMove(e, gestureState) {
-        // console.log(e)
-        console.log(gestureState.vx, gestureState.vy)
-    }
+  _handlePanResponderMove(e, gestureState) {
+    // console.log(e)
+    console.log(gestureState.vx, gestureState.vy)
+  }
 
-    _handlePanResponderEnd(e, gestureState) {
-        console.log('end')
-        console.log(gestureState)
-    }
+  _handlePanResponderEnd(e, gestureState) {
+    console.log('end')
+    console.log(gestureState)
+  }
 
-    render() {
-        return (
-            <View>
-                <TodoTextInput
-                    newTodo
-                    placeholder="What needs to be done?"
-                    onSave={this.onChangeText.bind(this)}
-                />
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View>
+        <TodoTextInput
+          newTodo
+          placeholder="What needs to be done?"
+          onSave={this.onChangeText.bind(this)}
+        />
+      </View>
+    )
+  }
 }
--- server.js
+++ server.js
@@ -9,24 +9,24 @@ var port = 3000
 
 var compiler = webpack(config)
 app.use(webpackDevMiddleware(compiler, {
-    noInfo: true,
-    publicPath: config.output.publicPath
+  noInfo: true,
+  publicPath: config.output.publicPath
 }))
 app.use(webpackHotMiddleware(compiler))
 
 app.get("/", function(req, res) {
-    res.sendFile(__dirname + '/index.html')
+  res.sendFile(__dirname + '/index.html')
 })
 
 app.get("/test", function(req, res) {
-    res.sendFile(__dirname + '/test/index.html')
+  res.sendFile(__dirname + '/test/index.html')
 })
 
 app.listen(port, function(error) {
-    if (error) {
-        console.error(error)
-    } else {
-        console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
-        exec('open http://localhost:' + port);
-    }
+  if (error) {
+    console.error(error)
+  } else {
+    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
+    exec('open http://localhost:' + port);
+  }
 })
